b88804e70c36d630c8e3491327cb0aa33fe49cde
Kim Yoo <kimyoo04eco@naver.com>
2023-01-06 14:25:21 +0900

    Update Home page content example

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,31 +6,31 @@ export default function Home() {
     <MainLayout>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-col text-center w-full mb-20">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+          <div className="flex flex-col w-full mb-20 text-center">
+            <h1 className="mb-4 text-2xl font-medium text-gray-900 sm:text-3xl title-font">
               Master Cleanse Reliac Heirloom
             </h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            <p className="mx-auto text-base leading-relaxed lg:w-2/3">
               Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
               gentrify, subway tile poke farm-to-table. Franzen you probably
               haven&apos;t heard of them man bun deep jianbing selfies heirloom.
             </p>
           </div>
           <div className="flex flex-wrap -m-4">
-            <div className="lg:w-1/3 sm:w-1/2 p-4">
-              <div className="flex relative">
+            <div className="p-4 lg:w-1/3 sm:w-1/2">
+              <div className="relative flex">
                 <Image
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 object-cover object-center w-full h-full"
                   src="https://dummyimage.com/600x360"
                   width={600}
                   height={360}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                <div className="relative z-10 w-full px-8 py-10 bg-white border-4 border-gray-200 opacity-0 hover:opacity-100">
+                  <h2 className="mb-1 text-sm font-medium tracking-widest text-indigo-500 title-font">
                     THE SUBTITLE
                   </h2>
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                  <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">
                     Shooting Stars
                   </h1>
                   <p className="leading-relaxed">
@@ -40,20 +40,20 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="lg:w-1/3 sm:w-1/2 p-4">
-              <div className="flex relative">
+            <div className="p-4 lg:w-1/3 sm:w-1/2">
+              <div className="relative flex">
                 <Image
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 object-cover object-center w-full h-full"
                   src="https://dummyimage.com/600x360"
                   width={600}
                   height={360}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                <div className="relative z-10 w-full px-8 py-10 bg-white border-4 border-gray-200 opacity-0 hover:opacity-100">
+                  <h2 className="mb-1 text-sm font-medium tracking-widest text-indigo-500 title-font">
                     THE SUBTITLE
                   </h2>
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                  <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">
                     The Catalyzer
                   </h1>
                   <p className="leading-relaxed">
@@ -63,20 +63,20 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="lg:w-1/3 sm:w-1/2 p-4">
-              <div className="flex relative">
+            <div className="p-4 lg:w-1/3 sm:w-1/2">
+              <div className="relative flex">
                 <Image
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 object-cover object-center w-full h-full"
                   src="https://dummyimage.com/600x360"
                   width={600}
                   height={360}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                <div className="relative z-10 w-full px-8 py-10 bg-white border-4 border-gray-200 opacity-0 hover:opacity-100">
+                  <h2 className="mb-1 text-sm font-medium tracking-widest text-indigo-500 title-font">
                     THE SUBTITLE
                   </h2>
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                  <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">
                     The 400 Blows
                   </h1>
                   <p className="leading-relaxed">
@@ -86,20 +86,20 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="lg:w-1/3 sm:w-1/2 p-4">
-              <div className="flex relative">
+            <div className="p-4 lg:w-1/3 sm:w-1/2">
+              <div className="relative flex">
                 <Image
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 object-cover object-center w-full h-full"
                   src="https://dummyimage.com/600x360"
                   width={600}
                   height={360}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                <div className="relative z-10 w-full px-8 py-10 bg-white border-4 border-gray-200 opacity-0 hover:opacity-100">
+                  <h2 className="mb-1 text-sm font-medium tracking-widest text-indigo-500 title-font">
                     THE SUBTITLE
                   </h2>
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                  <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">
                     Neptune
                   </h1>
                   <p className="leading-relaxed">
@@ -109,20 +109,20 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="lg:w-1/3 sm:w-1/2 p-4">
-              <div className="flex relative">
+            <div className="p-4 lg:w-1/3 sm:w-1/2">
+              <div className="relative flex">
                 <Image
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 object-cover object-center w-full h-full"
                   src="https://dummyimage.com/600x360"
                   width={600}
                   height={360}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                <div className="relative z-10 w-full px-8 py-10 bg-white border-4 border-gray-200 opacity-0 hover:opacity-100">
+                  <h2 className="mb-1 text-sm font-medium tracking-widest text-indigo-500 title-font">
                     THE SUBTITLE
                   </h2>
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                  <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">
                     Holden Caulfield
                   </h1>
                   <p className="leading-relaxed">
@@ -132,20 +132,20 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="lg:w-1/3 sm:w-1/2 p-4">
-              <div className="flex relative">
+            <div className="p-4 lg:w-1/3 sm:w-1/2">
+              <div className="relative flex">
                 <Image
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 object-cover object-center w-full h-full"
                   src="https://dummyimage.com/600x360"
                   width={600}
                   height={360}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                <div className="relative z-10 w-full px-8 py-10 bg-white border-4 border-gray-200 opacity-0 hover:opacity-100">
+                  <h2 className="mb-1 text-sm font-medium tracking-widest text-indigo-500 title-font">
                     THE SUBTITLE
                   </h2>
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                  <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">
                     Alper Kamu
                   </h1>
                   <p className="leading-relaxed">
