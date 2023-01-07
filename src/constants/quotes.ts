a7475bf75b073410c4a738e5930710cc19f32553
Kim Yoo <kimyoo04eco@naver.com>
2023-01-08 02:43:07 +0900

    Create quotes.ts

---
+++ src/constants/quotes.ts
@@ -0,0 +1,10 @@
+export const quotes = [
+  {
+    architect: "Antoni Gaudi",
+    quote: "자연은 신이 만든 건축이며 인간의 건축은 그것을 배워야 한다.",
+  },
+  {
+    architect: "Le Corbusier",
+    quote: "집은 살기 위한 기계다.",
+  },
+];
