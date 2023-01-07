8a1ded97ecb9c2d10c64cfcf75077a09fd1eeee0
Kim Yoo <kimyoo04eco@naver.com>
2023-01-07 18:54:25 +0900

    Fix svg error, link tag error

--- src/components/Layouts/Footer/Links.tsx
+++ src/components/Layouts/Footer/Links.tsx
@@ -51,7 +51,7 @@ export default function Links() {
         >
           <path
             stroke="none"
-            d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12Linkz"
+            d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
           ></path>
           <circle cx="4" cy="4" r="2" stroke="none"></circle>
         </svg>
