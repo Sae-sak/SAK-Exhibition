b1d7a355503cfffeb868acad037f0866e6a50d61
chae-zero <essentials106@ewhain.net>
2023-01-09 13:12:40 +0900

    Update quotes.ts

--- src/constants/quotes.ts
+++ src/constants/quotes.ts
@@ -7,4 +7,92 @@ export const quotes = [
     architect: "Le Corbusier",
     quote: "집은 살기 위한 기계다.",
   },
+  {
+    architect: "Le Corbusier",
+    quote: "건축은 빛과 함께할 때 정확하고 근사해지는 배운 자들의 게임이다.",
+  },
+  {
+    architect: "Constantin Brancusi",
+    quote: "건축은 누군가가 사는 조각이다.",
+  },
+  {
+    architect: "Ieoh Ming Pei",
+    quote: "삶은 건축이고 건축은 삶의 거울이다.",
+  },
+  {
+    architect: "Jean Nouvel",
+    quote: "새로운 상황은 매번 새로운 건축을 요구한다.",
+  },
+  {
+    architect: "Johann Wolfgang von Goethe",
+    quote: "건축이란 얼어붙은 음악이다.",
+  },
+  {
+    architect: "Winston Churchill",
+    quote: "우리는 건물을 만들고, 그 이후에는 건물이 우리를 만든다.",
+  },
+  {
+    architect: "Auguste Perret",
+    quote: "건축가는 '구조'라는 모국어를 통해 생각하고 자신을 표현하는 시인이다.",
+  },
+  {
+    architect: "Juhani Pallasmaa",
+    quote: "문의 손잡이는 건물이 건네는 악수이다.",
+  },
+  {
+    architect: "Louis Kahn",
+    quote: "벽돌조차도 무언가가 되고 싶어 한다.",
+  },
+  {
+    architect: "Louis Kahn",
+    quote: "건축은 진실에 대한 접근이다.",
+  },
+  {
+    architect: "Ludwig Mies van der Rohe",
+    quote: "건축은 두 벽돌을 신중하게 붙이는 순간 시작된다.",
+  },
+  {
+    architect: "Ludwig Mies van der Rohe",
+    quote: "신은 디테일에 존재한다.",
+  },
+  {
+    architect: "Luis Barragan",
+    quote: "평온함을 나타내지 않는 것은 건축에 있어 큰 착오이다.",
+  },
+  {
+    architect: "Norman Foster",
+    quote: "건축가로서 당신은, 본질적으로 알지 못하는 미래를 위해 과거를 의식하며 현재를 디자인한다.",
+  },
+  {
+    architect: "Norman Foster",
+    quote: "건축은 가치들의 표현이다.",
+  },
+  {
+    architect: "Renzo Piano",
+    quote: "건축의 큰 아름다움 중 하나는, 그것이 매번 처음부터 다시 시작되는 삶과 같다는 점이다."
+  },
+  {
+    architect: "Tadao Ando",
+    quote: "나는 사람들이 살아가는 방식이 건축에 의해 조금씩 변화할 수 있다고 믿는다.",
+  },
+  {
+    architect: "Vito Acconci",
+    quote: "건축은 공간에 대한 것이 아니라 시간에 대한 것이다.",
+  },
+  {
+    architect: "Glenn Murcutt",
+    quote: "우리는 일을 하지 않는다. 나는 우리가 발견자라고 믿는다.",
+  },
+  {
+    architect: "Eugene Viollet-le-duc",
+    quote: "건축물의 복원이란 그것을 과거의 어떤 시기에도 존재한 적 없었던, 하나의 통합된 상태로 만드는 것이다.",
+  },
+  {
+    architect: "Hendrik Petrus Berlage",
+    quote: "건축가의 임무는 외관을 스케치하는 것이 아니라, 공간을 창조하는 것이다.",
+  },
+  {
+    architect: "Rudolrh M. Schindler",
+    quote: "건축은 공간과 그 형태를 포착할 수 있는 아티스트에게 맡겨져야 한다.",
+  },
 ];
