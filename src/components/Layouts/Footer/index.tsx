4fd5da8a08412f2d9220c7ac337b3848b86e60b0
Kim Yoo <kimyoo04eco@naver.com>
2023-01-14 23:35:45 +0900

    Update Header, Footer component (apply font, hover:kgu_main_color)

--- src/components/Layouts/Footer/index.tsx
+++ src/components/Layouts/Footer/index.tsx
@@ -7,17 +7,23 @@ import Logo from "./FooterItem/Logo";
 export default function Footer() {
   return (
     <footer className="text-gray-600 body-font">
-      <div className="container flex-wrap justify-between px-5 py-12 mx-auto row-center md:items-center lg:items-start md:flex-row md:flex-nowrap">
-        <div className="flex flex-wrap flex-grow order-first text-center md:pr-20 md:text-left">
+      {/* main footer */}
+      <div className="container flex flex-wrap justify-between px-5 py-12 mx-auto md:items-center lg:items-start md:flex-row md:flex-nowrap">
+        {/* 네비게이션 */}
+        <div className="flex flex-wrap flex-grow order-first my-auto text-center lg:pl-4 md:pr-32 md:text-left ">
           <NavLink linkpath={""} name={"Home"} />
           <NavLink linkpath={"about"} name={"About"} />
           <NavLink linkpath={"exhibition"} name={"Exhibition"} />
           <NavLink linkpath={"contactus"} name={"Contact Us"} />
         </div>
-        <div className="flex-shrink-0 gap-6 pl-10 pr-5 mx-auto mt-5 text-center col-center md:mx-0 md:text-left md:mt-0">
+
+        {/* 로고 */}
+        <div className="gap-6 pr-4 mx-auto mt-5 text-center lg:pl-10 row-center md:mx-0 md:text-left md:mt-0">
           <Logo />
         </div>
       </div>
+
+      {/* sub footer */}
       <div className="bg-gray-200">
         <div className="container flex-wrap gap-2 px-5 py-4 mx-auto col-center sm:flex-row">
           <Links />
--- src/components/Layouts/Header/index.tsx
+++ src/components/Layouts/Header/index.tsx
@@ -5,14 +5,21 @@ import NavLink from "./HeaderItem/NavLink";
 export default function Header() {
   return (
     <header className="text-gray-600 body-font">
-      <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
-        <Logo />
-        <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto">
+      <div className="container flex-wrap gap-4 p-5 mx-auto row-center lg:justify-between sm:col-center">
+        <div className="gap-6 row-end">
+          {/* 로고홈 */}
+          <Logo />
+
+          {/* 토글 버튼 */}
+          <ThemeChanger />
+        </div>
+
+        {/* 네비게이션 */}
+        <nav className="text-base row-center sm:mt-auto ">
           <NavLink linkpath={""} name={"Home"} />
           <NavLink linkpath={"about"} name={"About"} />
           <NavLink linkpath={"exhibition"} name={"Exhibition"} />
           <NavLink linkpath={"contactus"} name={"Contact Us"} />
-          <ThemeChanger />
         </nav>
       </div>
     </header>
