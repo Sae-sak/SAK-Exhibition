3621e29456e47426941b027c7340a198073622e7
Kim Yoo <kimyoo04eco@naver.com>
2023-01-14 23:34:32 +0900

    Add font, color settings

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -86,6 +86,13 @@ export default function Document() {
         />
         <link rel="favicon" type="image/x-icon" href="/favicon/favicon.ico" />
         <link rel="/manifest" href="/favicon/manifest.json" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Barlow:wght@200;300;400;500;600;700&family=Bebas+Neue&family=Lustria&family=PT+Serif&family=Roboto:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
@@ -8,7 +10,32 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        kgu_main_orange: "#F25822",
+        kgu_main_yellow: "#F7941D",
+        kgu_main_blue: "#005AAB",
+        kgu_main_green: "#00AB5B",
+
+        sak_main_pink: "#FCDAB0",
+        sak_main_orange: "#F89F80",
+        sak_main_apricot: "#F57B1F",
+        sak_main_olivegreen: "#707E55",
+        sak_main_artemisiagreen: "#698741",
+
+        sak_point_green: "#A1E0C3",
+        sak_point_jungle: "#007D88",
+        sak_point_blue: "#95BADC",
+      },
+      fontFamily: {
+        header: ["Amagro", "serif"],
+        title: ["PT serif", "serif"],
+        subtitle: ["Lustria", "serif"],
+        body: ["Barlow", "sans-serif"],
+        button: ["Bebas Neue", "cursive"],
+        caption: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
