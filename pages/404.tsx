787b74631b52886dc93a6977c7b80a127a014424
Kim Yoo <kimyoo04eco@naver.com>
2023-01-18 20:26:03 +0900

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
+
 export default function NotFoundPage() {
   return (
-    <>
-      <div className="flex items-center justify-center w-screen h-screen">
-        <span className="text-xl">404 - Page Not Found</span>
+    <div className="w-screen h-screen gap-8 col-center">
+      <div className="gap-4 text-3xl font-bold row-center">
+        <span className="">S.A.K</span>
+        <span className="">|</span>
+        <span className="">404 - Page Not Found</span>
       </div>
-    </>
+      <Link href="/">
+        <button className="p-2 text-3xl border rounded">
+          <span>Go Home</span>
+        </button>
+      </Link>
+    </div>
   );
 }
