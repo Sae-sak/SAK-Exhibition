04d62d503b02c1a85780317848b3371901f40d70
Kim Yoo <kimyoo04eco@naver.com>
2023-01-19 00:10:49 +0900

    Create signin, signup route

---
+++ pages/signin/index.tsx
@@ -0,0 +1,10 @@
+import MainLayout from "@components/Layouts/MainLayout";
+import SigninPage from "@components/Pages/SigninPage";
+
+export default function Signin() {
+  return (
+    <MainLayout>
+      <SigninPage />
+    </MainLayout>
+  );
+}
