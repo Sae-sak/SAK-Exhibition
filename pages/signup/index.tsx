eccddc1938762fc1daa29f059f3dddccf59eafdb
Kim Yoo <kimyoo04eco@naver.com>
2023-01-20 17:16:49 +0900

    Update signup route

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,3 +1,15 @@
+import Alert from "@components/Base/Alert";
+import AuthLayout from "@components/Layouts/AuthLayout";
+import SignupPage from "@components/Pages/SignupPage";
+import {alertActions} from "@features/alert/alertSlice";
+import {useAppDispatch, useAppSelector} from "@toolkit/hook";
+
 export default function Signup() {
-  return <div>Signup</div>;
+  const dispatch = useAppDispatch();
+
+  return (
+    <AuthLayout>
+      <SignupPage />
+    </AuthLayout>
+  );
 }
