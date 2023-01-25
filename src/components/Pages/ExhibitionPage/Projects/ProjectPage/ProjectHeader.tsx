f31a91d0e0467f38d20c472926eba2ec4defac4e
Kim Yoo <kimyoo04eco@naver.com>
2023-01-25 23:46:01 +0900

    Implement Project page Header

--- src/components/Pages/ExhibitionPage/Projects/ProjectPage/ProjectHeader.tsx
+++ src/components/Pages/ExhibitionPage/Projects/ProjectPage/ProjectHeader.tsx
@@ -1,16 +1,33 @@
 import Image from "next/image";
 
-export default function ProjectHeader({title, thumbnailImg}) {
+export default function ProjectHeader({title, excerpt, thumbnailImg}) {
   return (
-    <header>
-      <h1>{title}</h1>
-      <Image
-        src={thumbnailImg}
-        alt={"thumbnail"}
-        width={300}
-        height={200}
-        priority
-      />
+    <header className="relative h-96">
+      {/* Project Title */}
+      <h1 className="absolute z-10 text-4xl font-bold text-white font-titleKR top-40 left-8">
+        {title}
+      </h1>
+
+      {/* Background Image */}
+      <div className="absolute w-full h-96 cover-fill">
+        <div
+          className="w-full h-full"
+          style={{backgroundImage: `url(${thumbnailImg})`}}
+        ></div>
+      </div>
+
+      {/* Gradient */}
+      <div
+        className="absolute w-full h-96"
+        style={{
+          backgroundImage:
+            "linear-gradient(to left, rgba(255,0,0,0), rgba(0,0,0,1))",
+        }}
+      ></div>
+
+      <div className="absolute z-10 pr-32 top-56 left-8">
+        <h3 className="text-xl text-white font-titleKR ">{excerpt}</h3>
+      </div>
     </header>
   );
 }
--- src/components/Pages/ExhibitionPage/Projects/ProjectPage/index.tsx
+++ src/components/Pages/ExhibitionPage/Projects/ProjectPage/index.tsx
@@ -4,13 +4,16 @@ import AnchorUrl from "./AnchorUrl";
 
 import {IProject} from "@constants/types/exhibition";
 import ProgressBar from "@components/Base/ProgressBar/ProgressBar";
+import Carousel from "@components/Pages/ExhibitionPage/Projects/ProjectPage/Carousel";
 import ModalImage from "./ModalImage";
 import StickyUrl from "./StickyUrl";
+import Comment from "./Comment";
 
 export default function ProjectPage({projectData}: {projectData: IProject}) {
   const {
     slug,
     title,
+    excerpt,
     date,
     year,
     semester,
@@ -23,34 +26,43 @@ export default function ProjectPage({projectData}: {projectData: IProject}) {
     content,
   } = projectData;
 
-  const thumbnailImg = `/posts-images/${slug.replaceAll(
-    "/",
-    "_"
-  )}/thumbnail.jpg`;
+  const imagePath = `/posts-images/${slug.replaceAll("/", "_")}`;
+  const thumbnailImg = `${imagePath}/thumbnail.jpg`;
+  const drawingImgs = [
+    `${imagePath}/draw1.jpg`,
+    `${imagePath}/draw2.jpg`,
+    `${imagePath}/draw3.jpg`,
+  ];
 
   return (
     <>
       <ProgressBar />
       <article className="container p-5 ">
-        <ProjectHeader title={title} thumbnailImg={thumbnailImg} />
+        <ProjectHeader
+          title={title}
+          excerpt={excerpt}
+          thumbnailImg={thumbnailImg}
+        />
 
         {/* body 세등분 */}
-        <div className="flex pt-24 flex-nowrap">
+        <section className="flex pt-24 flex-nowrap">
           {/* sticky URLs */}
-          <div className="sticky w-1/6 h-5 top-52">
+          <section className="sticky w-1/6 h-5 top-52">
             <StickyUrl />
-          </div>
+          </section>
 
           {/* content */}
-          <div className="w-2/3">
+          <section className="w-2/3">
             <ProjectContent content={content} />
-          </div>
+            <Carousel data={drawingImgs} />
+            <Comment />
+          </section>
 
           {/* anchor URLs */}
-          <div className="sticky w-1/6 h-5 top-52">
+          <section className="sticky w-1/6 h-5 top-52">
             <AnchorUrl content={content} />
-          </div>
-        </div>
+          </section>
+        </section>
       </article>
 
       <ModalImage />
