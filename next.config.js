818e7c4541d2c2227850169301f1e3f7b609a0d9
Kim Yoo <kimyoo04eco@naver.com>
2023-01-25 14:05:28 +0900

    Update config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,31 @@
+const {PHASE_DEVELOPMENT_SERVER} = require("next/dist/shared/lib/constants");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = {fs: false};
-    return config;
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "dummyimage.com",
-        port: "",
+const nextConfig = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      webpack: (config) => {
+        config.resolve.fallback = {fs: false};
+        return config;
       },
-    ],
-  },
+      env: {
+        markdownPath: "",
+      },
+    };
+  }
+
+  return {
+    reactStrictMode: true,
+    webpack: (config) => {
+      config.resolve.fallback = {fs: false};
+      return config;
+    },
+    env: {
+      markdownPath: "",
+    },
+  };
 };
+``;
 
 module.exports = nextConfig;
