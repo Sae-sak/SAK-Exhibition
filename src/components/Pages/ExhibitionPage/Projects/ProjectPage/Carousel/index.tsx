e9b1a61d2f1e38f01b6257580281e6b5efeae0a4
Kim Yoo <kimyoo04eco@naver.com>
2023-01-25 23:45:29 +0900

    Implement Carousel / Install react-use-measure

--- src/components/Pages/ExhibitionPage/Projects/ProjectPage/Carousel/index.tsx
+++ src/components/Pages/ExhibitionPage/Projects/ProjectPage/Carousel/index.tsx
@@ -1,3 +1,68 @@
-export default function Carousel() {
-  return <div></div>;
+import {useState} from "react";
+import {useAppDispatch} from "@toolkit/hook";
+import {photoActions} from "@features/photo/photoSlice";
+
+export default function Carousel(props: {data: string[]}) {
+  const data = props.data;
+  const dataLen = data.length;
+
+  const dispatch = useAppDispatch();
+  let [count, setCount] = useState(1);
+
+  function goToSlide(slideIndex: number) {
+    setCount(slideIndex);
+  }
+
+  return (
+    <div className="max-w-[1400px] h-[580px] w-full m-auto py-16 relative group">
+      <div
+        style={{backgroundImage: `url(${data[Math.abs(count) % dataLen]})`}}
+        className="w-full h-full duration-500 bg-center bg-cover rounded-2xl"
+        onClick={() =>
+          dispatch(
+            photoActions.openPhoto({
+              selectedId: data[Math.abs(count) % dataLen],
+            })
+          )
+        }
+      ></div>
+
+      {/* Left Arrow */}
+      <button
+        className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-2xl p-1 bg-black/70 text-white cursor-pointer"
+        onClick={() => setCount(count - 1)}
+      >
+        <i className="text-3xl ri-arrow-left-s-line"></i>
+      </button>
+
+      {/* Right Arrow */}
+      <button
+        className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-2xl p-1 bg-black/70 text-white cursor-pointer"
+        onClick={() => setCount(count + 1)}
+      >
+        <i className="text-3xl ri-arrow-right-s-line"></i>
+      </button>
+
+      {/* Dots */}
+      <div className="gap-2 p-4 row-center">
+        {data.map((_, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className="text-2xl cursor-pointer"
+          >
+            <div key={slideIndex} onClick={() => goToSlide(slideIndex)}>
+              <div
+                className={`w-2 h-2 ${
+                  Math.abs(count) % dataLen === slideIndex
+                    ? "bg-black"
+                    : "bg-gray-300"
+                } rounded-full`}
+              ></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Pages/ExhibitionPage/Projects/ProjectPage/Carousel/motionCarousel.tsx
@@ -0,0 +1,77 @@
+import {useState} from "react";
+import {AnimatePresence, motion} from "framer-motion";
+import useMeasure from "react-use-measure";
+import usePrevious from "@customHook/usePrevious";
+
+export default function Carousel(props: {data: string[]}) {
+  const data = props.data;
+  const dataLen = data.length;
+
+  let [ref, {width}] = useMeasure();
+  let [count, setCount] = useState(1);
+  let prev = usePrevious(count);
+  let direction = count > prev ? 1 : -1;
+
+  function goToSlide(slideIndex) {
+    setCount(slideIndex);
+  }
+
+  return (
+    <div className="col-center">
+      <div className="text-white bg-slate-500 row-center group">
+        <button onClick={() => setCount(count - 1)}>
+          <i className="text-3xl ri-arrow-left-s-line"></i>
+        </button>
+
+        <div className="col-center">
+          <div
+            ref={ref}
+            className="relative w-24 h-24 overflow-hidden bg-gray-700 row-center"
+          >
+            <AnimatePresence custom={{direction, width}}>
+              <motion.div
+                variants={carouselVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                custom={{direction, width}}
+                key={count}
+                className={`absolute w-20 h-20 bg-red-500 row-center ${
+                  data[Math.abs(count) % dataLen]
+                }`}
+              >
+                {Math.abs(count) % dataLen}
+              </motion.div>
+            </AnimatePresence>
+          </div>
+        </div>
+
+        <button onClick={() => setCount(count + 1)}>
+          <i className="text-3xl ri-arrow-right-s-line"></i>
+        </button>
+      </div>
+
+      <div className="gap-2 p-4 row-center">
+        {data.map((_: string, slideIndex: number) => {
+          return (
+            <div key={slideIndex} onClick={() => goToSlide(slideIndex)}>
+              <div
+                className={`w-2 h-2 ${
+                  Math.abs(count) % dataLen === slideIndex
+                    ? "bg-black"
+                    : "bg-gray-300"
+                } rounded-full`}
+              ></div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+let carouselVariants = {
+  hidden: ({direction, width}) => ({x: direction * width}),
+  visible: {x: 0},
+  exit: ({direction, width}) => ({x: direction * -width}),
+};
