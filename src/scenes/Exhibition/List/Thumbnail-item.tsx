a084f58aea2c321e9c362e4c430fb9122a8f4be6
Kim Yoo <kimyoo04eco@naver.com>
2023-01-30 14:28:41 +0900

    Implement Loading (Ongoing UI)

--- src/scenes/Exhibition/List/Thumbnail-item.tsx
+++ src/scenes/Exhibition/List/Thumbnail-item.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 import Image from "next/image";
 import Link from "next/link";
-import { motion } from "framer-motion";
+import { useRouter } from "next/router";
+
+import Loading from "@components/Loading";
+
 import { IProject } from "@type/exhibition";
 
 export default function ThumbnailItem({ project }: { project: IProject }) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
   const {
     slug,
     title,
@@ -23,32 +32,39 @@ export default function ThumbnailItem({ project }: { project: IProject }) {
   const profileImg = `/posts-images/${imagePath}/profile.jpg`;
 
   return (
-    <motion.div
-      className="w-full p-2 mb-12 rounded lg:w-1/3 md:w-1/2"
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      viewport={{ once: true }}
-      transition={{ duration: 0.6 }}
-    >
-      <Link
-        rel="preload"
-        href={`/exhibition/${slug}/`}
-        as={`/exhibition/${slug}/`}
+    <>
+      <motion.div
+        className="w-full p-2 mb-12 rounded lg:w-1/3 md:w-1/2"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.6 }}
       >
-        <Image
-          src={thumbnailImg}
-          alt={title}
-          width={900}
-          height={900}
-          object-fit="cover"
-          className="object-cover h-full"
-          loading="lazy"
-        />
-        <div className="h-14">
-          <span className="text-sm text-gray-500 font-titleKR">{name}</span>
-          <p className="text-gray-700 font-titleKR">{title}</p>
-        </div>
-      </Link>
-    </motion.div>
+        <Link
+          rel="preload"
+          href={`/exhibition/${slug}/`}
+          onClick={(e) => {
+            if (e.ctrlKey || e.metaKey) return;
+            e.preventDefault();
+            setIsLoading(true);
+            router.push(e.currentTarget.href);
+          }}
+        >
+          <Image
+            src={thumbnailImg}
+            alt={title}
+            width={400}
+            height={400}
+            className="object-cover h-full"
+            loading="lazy"
+          />
+          <div className="h-14">
+            <span className="text-sm text-gray-500 font-titleKR">{name}</span>
+            <p className="text-gray-700 font-titleKR">{title}</p>
+          </div>
+        </Link>
+      </motion.div>
+      {isLoading && <Loading />}
+    </>
   );
 }
