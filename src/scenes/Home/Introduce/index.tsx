db95976bf55c51ecdb20191d26694bd03a9d9495
Kim Yoo <kimyoo04eco@naver.com>
2023-02-01 00:52:12 +0900

    Update Home page scenarios height

--- src/scenes/Home/Introduce/index.tsx
+++ src/scenes/Home/Introduce/index.tsx
@@ -1,6 +1,6 @@
 export default function Introduce() {
   return (
-    <div className="py-4 border row-center">
+    <div className="h-screen py-4 border row-center">
       <h1 className="text-5xl">Introduce</h1>
     </div>
   );
--- src/scenes/Home/Points/index.tsx
+++ src/scenes/Home/Points/index.tsx
@@ -1,6 +1,6 @@
 export default function Points() {
   return (
-    <div className="py-4 border row-center">
+    <div className="h-screen py-4 border row-center">
       <h1 className="text-5xl">Points</h1>
     </div>
   );
