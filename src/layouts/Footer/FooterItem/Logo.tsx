a91edeed62c4d0b57c16d6ac85d2de781c48102d
Kim Yoo <kimyoo04eco@naver.com>
2023-02-09 18:43:51 +0900

    Update Header, Footer

--- src/layouts/Footer/FooterItem/Logo.tsx
+++ src/layouts/Footer/FooterItem/Logo.tsx
@@ -1,11 +1,15 @@
-import Image from "next/image";
-import logo from "public/favicon/favicon.png";
-
 export default function Logo() {
   return (
-    <div className="font-medium text-gray-900 row-center title-font md:justify-start">
-      <Image src={logo} alt="Home" width={32} height={32} />
-      <h1 className="ml-3 text-xl">SAK Exhibtion</h1>
+    <div className="relative w-[72px]">
+      <div className="z-30 block">
+        <span className="text-2xl font-bold text-kgu_main_red">S</span>
+        <span className="text-2xl font-bold text-kgu_main_yellow">A</span>
+        <span className="text-[25px] font-bold text-kgu_main_green">K</span>
+        <span>-</span>
+        <span className="text-[25px] font-bold text-kgu_main_blue">E</span>
+      </div>
+      <div className="absolute top-[8px] h-[1px] w-full bg-light_gray_1 dark:bg-night_gray_1" />
+      <div className="absolute bottom-[7px] h-[1px] w-full bg-light_gray_1 dark:bg-night_gray_1" />
     </div>
   );
 }
--- src/layouts/Footer/SimpleFooter.tsx
+++ src/layouts/Footer/SimpleFooter.tsx
@@ -4,14 +4,12 @@ import Copywrite from "./FooterItem/Copywrite";
 
 export default function SimpleFooter() {
   return (
-    <footer className="text-gray-600 body-font">
+    <footer className="border-t border-t-light_gray_3/30 dark:border-t-night_gray_3/30">
       {/* sub footer */}
-      <div className="bg-gray-200">
-        <div className="container flex-wrap gap-2 px-5 py-4 mx-auto col-center sm:flex-row">
-          <KGULinks />
-          <Copywrite />
-          <SNS />
-        </div>
+      <div className="container flex-wrap gap-2 px-5 py-4 mx-auto col-center sm:flex-row">
+        <KGULinks />
+        <Copywrite />
+        <SNS />
       </div>
     </footer>
   );
