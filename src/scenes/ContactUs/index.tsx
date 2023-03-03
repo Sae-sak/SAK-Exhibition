484b6d49ae29383932c7ad09deba31575463ab3b
Kim Yoo <kimyoo04eco@naver.com>
2023-03-03 18:34:46 +0900

    Update Contactus page

--- src/scenes/ContactUs/index.tsx
+++ src/scenes/ContactUs/index.tsx
@@ -1,10 +1,15 @@
+import HBorder from "@components/HBorder";
 import ContactUsForm from "./ContactUsForm";
+import StudentCouncil from "./StudentCouncil";
 import Team from "./Team/Index";
 
 export default function ContactUs() {
   return (
-    <div>
+    <div className="container gap-20 px-5 py-16 col-center ">
+      <StudentCouncil />
+      <HBorder />
       <Team />
+      <HBorder />
       <ContactUsForm />
     </div>
   );
