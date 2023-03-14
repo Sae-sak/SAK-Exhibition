77c9cf1e3da7adfff44d0e6507f088fbf28ce3cb
Kim Yoo <kimyoo04eco@naver.com>
2023-03-14 11:47:59 +0900

    Seperate colorVariants file

---
+++ src/util/Theme/colorVariants.ts
@@ -0,0 +1,26 @@
+export const colorVariants = {
+  red: {
+    textActive: "text-kgu_main_red ",
+    textHover: "group-hover:text-kgu_main_red",
+    borderActive: "w-48 bg-kgu_main_red ",
+    borderHover: "group-hover:bg-kgu_main_red",
+  },
+  yellow: {
+    textActive: "text-kgu_main_yellow ",
+    textHover: "group-hover:text-kgu_main_yellow",
+    borderActive: "w-48 bg-kgu_main_yellow ",
+    borderHover: "group-hover:bg-kgu_main_yellow",
+  },
+  blue: {
+    textActive: "text-kgu_main_blue ",
+    textHover: "group-hover:text-kgu_main_blue",
+    borderActive: "w-48 bg-kgu_main_blue ",
+    borderHover: "group-hover:bg-kgu_main_blue",
+  },
+  green: {
+    textActive: "text-kgu_main_green ",
+    textHover: "group-hover:text-kgu_main_green",
+    borderActive: "w-48 bg-kgu_main_green ",
+    borderHover: "group-hover:bg-kgu_main_green",
+  },
+};
