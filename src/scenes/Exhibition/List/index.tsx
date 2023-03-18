82c5c87116322f4c2414f78977a79303396d6980
Kim Yoo <kimyoo04eco@naver.com>
2023-03-18 23:28:35 +0900

    Fix ui error when filter one item

--- src/scenes/Exhibition/List/index.tsx
+++ src/scenes/Exhibition/List/index.tsx
@@ -3,7 +3,7 @@ import ThumbnailItem from "./Thumbnail-item";
 
 export default function List({ projects }: { projects: IProject[] }) {
   return (
-    <ul className="flex flex-wrap ">
+    <ul className="flex flex-wrap w-full">
       {projects.map((project: IProject) => (
         <ThumbnailItem project={project} key={project.slug} />
       ))}
