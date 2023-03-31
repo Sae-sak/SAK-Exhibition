a9260280435be69d065d51e361c21a5f937d8b4a
Kim Yoo <kimyoo04eco@naver.com>
2023-03-31 23:58:41 +0900

    Implement text hover random KGU color (at FilterItem)

--- src/scenes/Exhibition/FilterScreen/FilterComponents/FilterItem.tsx
+++ src/scenes/Exhibition/FilterScreen/FilterComponents/FilterItem.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 import { StudioKRType, StudioType, TagType } from "@type/exhibition";
 import { STUDENT, STUDIO, TAG } from "@constants/filter";
-import { motion } from "framer-motion";
 
 import {
   enableStudentFilter,
@@ -23,12 +25,24 @@ export default function FilterItem({
   const dispatch = useAppDispatch();
   const categoryName = useAppSelector((state) => state.filter.categoryName);
 
+  const [indx, setIndx] = useState(0);
+  const KGUColor = ["", "#E9511D", "#F49800", "#009E59", "#0050A0"];
+
+  function onRandomKGUColor() {
+    setIndx(Math.floor(((Math.random() * 10) % 4) + 1));
+  }
+  function onResetColor() {
+    setTimeout(() => setIndx(0), 200);
+  }
+
   return (
     <motion.button
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="cursor-pointer w-28"
+      className={`w-20 h-8`}
+      onMouseEnter={onRandomKGUColor}
+      onMouseLeave={onResetColor}
       onClick={() => {
         categoryName === STUDENT
           ? dispatch(enableStudentFilter({ studentName }))
@@ -39,14 +53,10 @@ export default function FilterItem({
           : null;
       }}
     >
-      <span>
-        {studentName !== ""
-          ? studentName
-          : studioName !== ""
-          ? studioNameKR
-          : tagName !== ""
-          ? tagName
-          : null}
+      <span style={{ color: `${KGUColor[indx]}` }}>
+        {studentName !== "" && studentName}
+        {studioName !== "" && studioNameKR}
+        {tagName !== "" && tagName}
       </span>
     </motion.button>
   );
