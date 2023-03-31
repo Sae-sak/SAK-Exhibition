04c15e905606bc86667d5040d810ccf4fffda133
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-03-31 12:19:34 +0900

    Update Contactus Page Clock

--- src/scenes/ContactUs/Clock/index.tsx
+++ src/scenes/ContactUs/Clock/index.tsx
@@ -1,15 +1,28 @@
-export default function Clock() {
-  // const clock = document.querySelector("#clock");
+import React, { useEffect, useState } from "react";
 
-  // const date = new Date();
-  // const hours = String(date.getHours()).padStart(2, "0");
-  // const seconds = String(date.getSeconds()).padStart(2, "0");
-  // const minutes = String(date.getMinutes()).padStart(2, "0");
-  // clock.innerText = `${hours}:${minutes}:${seconds}`; // 반드시 백틱 사용
+export default function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalID);
+  }, []);
+
+  const hours = time.getHours();
+  const ampm = hours >= 12 ? "PM" : "AM";
+  const displayHours = hours % 12 || 12;
+  const displayMinutes = time.getMinutes().toString().padStart(2, "0");
+  const displaySeconds = time.getSeconds().toString().padStart(2, "0");
 
   return (
     <div className=" w-full col-start gap-4 ">
-      <span className=" font-extrabold text-3xl">00:03:16 (UTC+09:00)</span>
+      <div className=" font-extrabold text-3xl">
+        {`${ampm}  ${displayHours}:${displayMinutes}:${displaySeconds} `}
+      </div>
+      <span className=" font-extrabold text-3xl">(UTC+09:00)</span>
       <div className=" font-semibold">Kimyoo04eco@naver.com</div>
       <div className=" font-semibold">010-8131-5224</div>
     </div>
