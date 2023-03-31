2282d84f2237d021078574b2d0c68389db658d33
Kim Yoo <kimyoo04eco@naver.com>
2023-04-01 01:57:30 +0900

    Delete ScrollDownButton

--- src/components/ScrollButton/index.tsx
+++ src/components/ScrollButton/index.tsx
@@ -8,7 +8,6 @@ import {
 
 export default function ScrollButton() {
   const scrollTopAni = useAnimation();
-  const scrollBottomAni = useAnimation();
 
   const { scrollY } = useScroll();
 
@@ -18,39 +17,25 @@ export default function ScrollButton() {
       position + window.innerHeight + 20 < document.body.scrollHeight
     ) {
       scrollTopAni.start("middle");
-      scrollBottomAni.start("middle");
     } else if (position !== 0) {
-      scrollBottomAni.start("bottom");
       scrollTopAni.start("middle");
     } else {
       scrollTopAni.start("top");
-      scrollBottomAni.start("middle");
     }
   });
 
   const scrollToTop = () => {
     window.scrollTo(0, 0);
   };
-  const scrollToBottom = () => {
-    window.scrollTo(0, window.innerHeight + document.body.scrollHeight);
-  };
 
   return (
-    <div className="fixed z-20 gap-3 bottom-4 right-4 col-center ">
+    <div className="fixed z-20 gap-3 bottom-16 right-4 col-center ">
       <motion.div variants={scrollTo.top} animate={scrollTopAni}>
         <button
           onClick={() => scrollToTop()}
           className="w-8 h-8 bg-white rounded-full shadow-sm shadow-gray-400 dark:bg-black row-center"
         >
           <i className="text-3xl ri-arrow-up-s-line"></i>
-        </button>
-      </motion.div>
-      <motion.div variants={scrollTo.bottom} animate={scrollBottomAni}>
-        <button
-          onClick={() => scrollToBottom()}
-          className="w-8 h-8 bg-white rounded-full shadow-sm shadow-gray-400 dark:bg-black row-center"
-        >
-          <i className="text-3xl ri-arrow-down-s-line"></i>
         </button>
       </motion.div>
     </div>
