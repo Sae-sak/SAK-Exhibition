e6dc882b39523ca73b4cab05c2c0bba9293714bb
Kim Yoo <kimyoo04eco@naver.com>
2023-04-02 02:01:43 +0900

    Refactoring fixed Component, MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,6 +4,10 @@ import Header from "@layouts/Header";
 import Footer from "@layouts/Footer";
 
 import ScrollButton from "@components/ScrollButton";
+import ChatButton from "@components/Chat/ChatButton";
+
+import Alert from "@components/Alert";
+import Chat from "@components/Chat";
 
 export default function MainLayout({ children }) {
   return (
@@ -13,10 +17,19 @@ export default function MainLayout({ children }) {
       exit={{ opacity: 0 }}
     >
       <Header />
+
       <div className="pt-[72px] flex flex-col min-h-[826px] items-center justify-start">
         {children}
       </div>
+
+      {/* 오른쪽 하단 fixed 버튼 */}
+      <ChatButton />
       <ScrollButton />
+
+      {/* overlay 되는 창 */}
+      <Alert />
+      <Chat />
+
       <Footer />
     </motion.div>
   );
