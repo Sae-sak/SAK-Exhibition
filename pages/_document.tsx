29b2f9afdfdf7a3d1eb876aa639ba86eaea83bcb
Kim <58503130+kimyoo04@users.noreply.github.com>
2023-04-01 18:11:58 +0900

    Fix z-index order (portal, chat)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -88,8 +88,8 @@ export default function Document() {
         <link rel="/manifest" href="/favicon/manifest.json" />
       </Head>
       <body>
+        <div className="fixed z-40 bottom-12 right-16 " id="chat"></div>
         <div className="fixed top-0 left-0 z-40 " id="portal"></div>
-        <div className="fixed z-40 bottom-16 right-16 " id="chat"></div>
         <Main />
         <NextScript />
       </body>
