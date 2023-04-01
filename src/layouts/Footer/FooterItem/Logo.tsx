618abd46ebf5b76c2c178cbc6a2d06fbfe61f65b
Kim Yoo <kimyoo04eco@naver.com>
2023-04-02 01:56:23 +0900

    Update whileTap whileHover animation / Fix darkmode font color

--- src/layouts/Footer/FooterItem/Logo.tsx
+++ src/layouts/Footer/FooterItem/Logo.tsx
@@ -7,11 +7,19 @@ export default function Logo() {
         <div className="bottom-[8px] h-[1px] w-full bg-light_gray_1 dark:bg-night_gray_1" />
       </div>
       <div className="absolute w-[72px] z-20">
-        <span className="text-2xl font-bold text-kgu_main_red">S</span>
-        <span className="text-2xl font-bold text-kgu_main_yellow">A</span>
-        <span className="text-2xl font-bold text-kgu_main_green">K</span>
+        <span className="text-2xl font-bold text-kgu_main_red dark:text-kgu_main_red">
+          S
+        </span>
+        <span className="text-2xl font-bold text-kgu_main_yellow dark:text-kgu_main_yellow">
+          A
+        </span>
+        <span className="text-2xl font-bold text-kgu_main_green dark:text-kgu_main_green">
+          K
+        </span>
         <span className="">-</span>
-        <span className="text-2xl font-bold text-kgu_main_blue">E</span>
+        <span className="text-2xl font-bold text-kgu_main_blue dark:text-kgu_main_blue">
+          E
+        </span>
       </div>
     </div>
   );
--- src/layouts/Header/HeaderItem/Logo.tsx
+++ src/layouts/Header/HeaderItem/Logo.tsx
@@ -9,33 +9,26 @@ export default function Logo() {
         <div className="w-[72px] z-20 h-4"></div>
         <div className="bottom-[8px] h-[1px] w-full bg-light_gray_1 dark:bg-night_gray_1" />
       </div>
-      <div className="absolute w-[72px] z-20">
-        <motion.span
-          layoutId="S"
-          className="text-2xl font-bold text-kgu_main_red dark:text-kgu_main_red"
-        >
+      <motion.div
+        whileTap={{
+          scale: 0.9,
+        }}
+        className="absolute w-[72px] z-20"
+      >
+        <span className="text-2xl font-bold text-kgu_main_red dark:text-kgu_main_red">
           S
-        </motion.span>
-        <motion.span
-          layoutId="A"
-          className="text-2xl font-bold text-kgu_main_yellow dark:text-kgu_main_yellow"
-        >
+        </span>
+        <span className="text-2xl font-bold text-kgu_main_yellow dark:text-kgu_main_yellow">
           A
-        </motion.span>
-        <motion.span
-          layoutId="K"
-          className="text-2xl font-bold text-kgu_main_green dark:text-kgu_main_green"
-        >
+        </span>
+        <span className="text-2xl font-bold text-kgu_main_green dark:text-kgu_main_green">
           K
-        </motion.span>
-        <span className="">-</span>
-        <motion.span
-          layoutId="E"
-          className="text-2xl font-bold text-kgu_main_blue dark:text-kgu_main_blue"
-        >
+        </span>
+        <span>-</span>
+        <span className="text-2xl font-bold text-kgu_main_blue dark:text-kgu_main_blue">
           E
-        </motion.span>
-      </div>
+        </span>
+      </motion.div>
     </Link>
   );
 }
