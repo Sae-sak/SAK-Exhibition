f1344dfd23a7611ea0f30a5011dd18f23ba5412e
Kim Yoo <kimyoo04eco@naver.com>
2023-06-03 19:20:31 +0900

    css: Logo 디자인에 아웃라인 텍스트 추가

--- src/layouts/Footer/FooterItem/Logo.tsx
+++ src/layouts/Footer/FooterItem/Logo.tsx
@@ -7,11 +7,12 @@ export default function Logo() {
         <div className="bottom-[8px] h-[1px] w-full bg-light_gray_1 dark:bg-night_gray_1" />
       </div>
       <div className="absolute w-[66px] z-10 font-accent">
-        <span className="text-2xl font-bold ">S</span>
-        <span className="text-2xl font-bold ">A</span>
-        <span className="text-2xl font-bold ">K</span>
-        <span>-</span>
-        <span className="text-2xl font-bold ">E</span>
+        <div className="relative">
+          <span className="text-2xl font-bold">SAK-E</span>
+          <span className="absolute -left-[1px] text-2xl font-bold text-outline">
+            SAK-E
+          </span>
+        </div>
       </div>
     </div>
   );
