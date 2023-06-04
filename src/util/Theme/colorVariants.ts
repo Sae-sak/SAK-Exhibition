7376143c3676b399107c82dd50fccd731f626a0e
Kim Yoo <kimyoo04eco@naver.com>
2023-06-05 03:00:11 +0900

    css: 네비게이션 링크의 폰트스타일, 폰트크기 조절

--- src/util/Theme/colorVariants.ts
+++ src/util/Theme/colorVariants.ts
@@ -1,6 +1,6 @@
 export const colorVariants = {
   textActive: "text-accent_pink/70 dark:text-accent_pink/70",
   textHover: "group-hover:text-accent_pink dark:group-hover:text-accent_pink",
-  borderActive: "w-48 bg-accent_pink/70 dark:bg-accent_pink/70",
+  borderActive: "w-32 md:w-36 bg-accent_pink/70 dark:bg-accent_pink/70",
   borderHover: "group-hover:bg-accent_pink dark:group-hover:bg-accent_pink",
 };
