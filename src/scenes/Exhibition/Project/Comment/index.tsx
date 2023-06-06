bf1663da8aad7c56f96b84590c37aaf7f4ee1572
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-06-06 18:07:09 +0900

    feat: Comment 컴포넌트 생성 및 Auth 인증 유무에 따른 UI 구현

--- src/scenes/Exhibition/Project/Comment/index.tsx
+++ src/scenes/Exhibition/Project/Comment/index.tsx
@@ -1,3 +1,45 @@
-export default function Comment() {
-  return <div className="w-full h-16 bg-slate-500"></div>;
+import CommentInput from "./CommentInput";
+import { useAppSelector } from "@toolkit/hook";
+import { useAuthState } from "react-firebase-hooks/auth";
+import LoadingIcon from "@components/Chat/LoadingIcon";
+import SignOut from "@components/Chat/Signout";
+import SignIn from "@components/Chat/Signin";
+import { auth } from "@config/firebaseApp";
+
+export default function Comment({
+  name,
+  slug,
+}: {
+  name: string;
+  slug: string;
+}) {
+  const isChat = useAppSelector((state) => state.chat.isChat);
+  const showChat = isChat ? "" : "opacity-0 scale-0 -z-10";
+
+  const [user, loading, error] = useAuthState(auth);
+
+  if (loading) return <LoadingIcon />;
+  if (error) return <p>error occur</p>;
+
+  return (
+    <div className="w-full bg-slate-100 border rounded-md p-4">
+      <div>방명록에 응원의 한 마디를 남겨보세요.</div>
+      {user && (
+        <div className="w-full col-end px-1">
+          <SignOut />
+        </div>
+      )}
+      {user ? (
+        // 댓글 입력 필드
+        <CommentInput />
+      ) : (
+        <div className="m-2 p-2">
+          <SignIn />
+        </div>
+      )}
+
+      {/* 댓글 목록 */}
+      <div className="w-full h-48 border-y bg-slate-300">Comment Lists</div>
+    </div>
+  );
 }
