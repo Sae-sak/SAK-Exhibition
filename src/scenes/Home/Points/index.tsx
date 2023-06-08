bb9912f45a763e7bd877e6b15ba5b93da2351760
Kim Yoo <kimyoo04eco@naver.com>
2023-06-09 02:11:01 +0900

    feat: 스튜디오명, 학생명 데이터로 Points 컴포넌트에 적용

--- src/scenes/Home/Points/index.tsx
+++ src/scenes/Home/Points/index.tsx
@@ -1,7 +1,25 @@
+import { playerData } from "public/data/playerData";
+
 export default function Points() {
   return (
-    <div className="h-screen py-4 border row-center">
-      <h1 className="text-5xl">Points</h1>
+    <div className="py-8 border-b flex flex-col gap-8 justify-between">
+      <h1 className="text-3xl">Player List</h1>
+      <div className="col-center gap-10">
+        {Object.keys(playerData).map((studio) => (
+          <div
+            key={studio}
+            className={`w-full flex flex-col justify-center items-start gap-1`}
+          >
+            <h4 className="text-xl">{studio}</h4>
+
+            <ul className="w-full flex items-start justify-start gap-2 flex-wrap">
+              {playerData[studio].map((player: string) => (
+                <li key={studio + player}>{player}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
