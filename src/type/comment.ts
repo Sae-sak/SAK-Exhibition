da0b886e186ae251728035fb9980fb5a5c9854ed
chae-zero <115343657+chae-zero@users.noreply.github.com>
2023-06-11 16:47:22 +0900

    feat: IComment 타입에 slug, commentId 추가

--- src/type/comment.ts
+++ src/type/comment.ts
@@ -1,9 +1,12 @@
 export interface IComment {
   uid: string;
+  commentId: string;
   displayName: string;
   text: string;
   photoURL: string;
   createdAt: { seconds: number; nanoseconds: number };
+
+  slug: string;
 }
 
 export interface ICommentForm {
