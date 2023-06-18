4f013772d59f3f7c1961ca6ec3b4a9cc4c60cd75
Kim <58503130+kimyoo04@users.noreply.github.com>
2023-06-18 23:22:30 +0900

    delete: 그리드 배경 이미지 제거

--- src/layouts/Header/Menu/Overlay.tsx
+++ src/layouts/Header/Menu/Overlay.tsx
@@ -1,12 +1,6 @@
-import bg from "../../../../public/images/grid-pattern-gray.png";
-
 export default function Overlay({ setIsClicked }) {
   return (
     <div
-      style={{
-        backgroundImage: `url(${bg.src})`,
-        backgroundRepeat: "repeat",
-      }}
       onClick={() => setIsClicked(false)}
       className="fixed top-0 z-20 w-screen h-screen bg-light_bg_1 col-center dark:bg-night_bg_1"
     ></div>
