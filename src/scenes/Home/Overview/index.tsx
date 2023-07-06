0628ec37d5e08d0942b48aa4bc9507bcc30bdae4
Kim Yoo <kimyoo04eco@naver.com>
2023-07-06 20:39:17 +0900

    fix: 네이버 지도 API 활성화

--- src/scenes/Home/Overview/index.tsx
+++ src/scenes/Home/Overview/index.tsx
@@ -6,25 +6,25 @@ import { textVariant } from "@util/variants";
 export default function Overview() {
   const mapElement = useRef(null);
 
-  // useEffect(() => {
-  //   const { naver } = window;
-  //   if (!mapElement.current || !naver) return;
-  //   // 지도에 표시할 위치의 위도와 경도 좌표를 파라미터로 넣어줍니다.
-  //   const location = new naver.maps.LatLng(37.5176, 126.9577);
-  //   const mapOptions: naver.maps.MapOptions = {
-  //     center: location,
-  //     zoom: 17,
-  //     zoomControl: true,
-  //     zoomControlOptions: {
-  //       position: naver.maps.Position.TOP_RIGHT,
-  //     },
-  //   };
-  //   const map = new naver.maps.Map(mapElement.current, mapOptions);
-  //   new naver.maps.Marker({
-  //     position: location,
-  //     map,
-  //   });
-  // }, []);
+  useEffect(() => {
+    const { naver } = window;
+    if (!mapElement.current || !naver) return;
+    // 지도에 표시할 위치의 위도와 경도 좌표를 파라미터로 넣어줍니다.
+    const location = new naver.maps.LatLng(37.5176, 126.9577);
+    const mapOptions: naver.maps.MapOptions = {
+      center: location,
+      zoom: 17,
+      zoomControl: true,
+      zoomControlOptions: {
+        position: naver.maps.Position.TOP_RIGHT,
+      },
+    };
+    const map = new naver.maps.Map(mapElement.current, mapOptions);
+    new naver.maps.Marker({
+      position: location,
+      map,
+    });
+  }, []);
 
   return (
     <div className="container col-center py-20">
