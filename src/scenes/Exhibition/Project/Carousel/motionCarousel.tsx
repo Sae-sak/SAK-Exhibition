686089e49b21d464fb5c6e59f6c685d83a8e2695
Kim Yoo <kimyoo04eco@naver.com>
2023-07-15 15:35:23 +0900

    style: type 에러 수정

--- src/scenes/Exhibition/Project/Carousel/motionCarousel.tsx
+++ src/scenes/Exhibition/Project/Carousel/motionCarousel.tsx
@@ -7,10 +7,10 @@ export default function Carousel(props: { data: string[] }) {
   const data = props.data;
   const dataLen = data.length;
 
-  let [ref, { width }] = useMeasure();
-  let [count, setCount] = useState(1);
-  let prev = usePrevious(count);
-  let direction = count > prev ? 1 : -1;
+  const [ref, { width }] = useMeasure();
+  const [count, setCount] = useState(1);
+  const prev = usePrevious(count);
+  const direction = count > prev ? 1 : -1;
 
   function goToSlide(slideIndex) {
     setCount(slideIndex);
@@ -18,15 +18,15 @@ export default function Carousel(props: { data: string[] }) {
 
   return (
     <div className="col-center">
-      <div className="text-white bg-slate-500 row-center group">
+      <div className="row-center group bg-slate-500 text-white">
         <button onClick={() => setCount(count - 1)}>
-          <i className="text-3xl ri-arrow-left-s-line"></i>
+          <i className="ri-arrow-left-s-line text-3xl"></i>
         </button>
 
         <div className="col-center">
           <div
             ref={ref}
-            className="relative w-24 h-24 overflow-hidden bg-gray-700 row-center"
+            className="row-center relative h-24 w-24 overflow-hidden bg-gray-700"
           >
             <AnimatePresence custom={{ direction, width }}>
               <motion.div
@@ -36,7 +36,7 @@ export default function Carousel(props: { data: string[] }) {
                 exit="exit"
                 custom={{ direction, width }}
                 key={count}
-                className={`absolute w-20 h-20 bg-red-500 row-center ${
+                className={`row-center absolute h-20 w-20 bg-red-500 ${
                   data[Math.abs(count) % dataLen]
                 }`}
               >
@@ -47,16 +47,16 @@ export default function Carousel(props: { data: string[] }) {
         </div>
 
         <button onClick={() => setCount(count + 1)}>
-          <i className="text-3xl ri-arrow-right-s-line"></i>
+          <i className="ri-arrow-right-s-line text-3xl"></i>
         </button>
       </div>
 
-      <div className="gap-2 p-4 row-center">
+      <div className="row-center gap-2 p-4">
         {data.map((_: string, slideIndex: number) => {
           return (
             <div key={slideIndex} onClick={() => goToSlide(slideIndex)}>
               <div
-                className={`w-2 h-2 ${
+                className={`h-2 w-2 ${
                   Math.abs(count) % dataLen === slideIndex
                     ? "bg-black"
                     : "bg-gray-300"
@@ -70,7 +70,7 @@ export default function Carousel(props: { data: string[] }) {
   );
 }
 
-let carouselVariants = {
+const carouselVariants = {
   hidden: ({ direction, width }) => ({ x: direction * width }),
   visible: { x: 0 },
   exit: ({ direction, width }) => ({ x: direction * -width }),
